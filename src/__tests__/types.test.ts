7c31b7efbc4ac96642983cd19de026ae06cf10b5
Alessandro Franchin <alessandro.franchin@hotmail.it>
2020-06-15 19:27:09 +0200

    test: aggiungi test classe Data

--- src/__tests__/types.test.ts
+++ src/__tests__/types.test.ts
@@ -1,5 +1,45 @@
-import {  Predictor } from '../panels/prediction-panel/types/Predictor';
+import { Predictor } from '../panels/prediction-panel/types/Predictor';
+import { GrafanaData } from '../panels/prediction-panel/types/Data'
+import { ArrayVector, DataFrame, FieldType, Field } from '@grafana/data';
 
+let testData: DataFrame[] = [
+    {
+        fields: [
+            {
+                name: '',
+                type: FieldType.time,
+                config: {},
+                values: new ArrayVector([123,321,456])
+            },
+            {
+                name: '',
+                type: FieldType.number,
+                config: {},
+                values: new ArrayVector([6,6,2])
+            }
+        ],
+        length: 10
+    },
+    {
+        fields: [
+            {
+                name: '',
+                type: FieldType.time,
+                config: {},
+                values: new ArrayVector([123,321,456])
+            },
+            {
+                name: '',
+                type: FieldType.number,
+                config: {},
+                values: new ArrayVector([7,3,0])
+            }
+        ],
+        length: 10
+    }
+];
+
+//Test Predictor class
 test('predictor RL well formed', () => {
     let predictor = new Predictor("RL", 69, [3,2,5,7],[4,2,0],undefined);
     const json = Predictor.readJson('{"type": "RL","tuples": 69,"coefficients": [3,2,5,7],"svmW": [4,2,0]}');
@@ -21,3 +61,15 @@ test('predictor empty', () => {
         Predictor.readJson('');
     }).toThrowError('Seleziona prima il file!');
 })
+
+//Test Data class
+test('inputGrafanaValues', () =>{
+    const data = GrafanaData.readValues(testData);
+    expect(data.inputGrafanaValues[0]).toStrictEqual([6,7,123]);
+    expect(data.inputGrafanaValues[1]).toStrictEqual([6,3,321]);
+    expect(data.inputGrafanaValues[2]).toStrictEqual([2,0,456]);
+})
+
+
+
+
